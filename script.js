933a3be7d8f5a9720b0510c9fff068a62b9c3171
metehan <shoghly25@gmail.com>
2022-08-14 14:31:45 +0300

    commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const nav = document.getElementById('nav');
+
+const Btn = document.getElementById('toggle');
+
+Btn.addEventListener('click', ()=>{
+    nav.classList.toggle('active');
+    
+})
